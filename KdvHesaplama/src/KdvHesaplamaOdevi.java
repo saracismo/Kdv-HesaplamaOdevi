ef60f37f2a01094673b1bd7b63f63be6be0a0dab
Mert Saraç <116085257+saracismo@users.noreply.github.com>
2023-09-14 10:19:23 +0300

    Kdv Hesaplama Projesi

---
+++ KdvHesaplama/src/KdvHesaplamaOdevi.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class KdvHesaplamaOdevi {
+    public static void main(String[] args) {
+        double tutar, kdvOran = 0.18, kdvTutar, kdvliTutar;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ücret Tutarını Giriniz :  ");
+        tutar = input.nextDouble();
+
+        kdvTutar = tutar * kdvOran;
+        kdvliTutar = tutar + kdvTutar;
+
+        System.out.println("KDV'siz Tutar :" + tutar);
+        System.out.println("KDV Oranı :  " + kdvOran);
+        System.out.println("KDV Tutarı:  " + kdvTutar);
+        System.out.println("KDV'li Tutar:  " + kdvliTutar);
+
+        boolean totalToplam1 = tutar > 0 && tutar <= 1000;
+        kdvOran = 0.18; // %18 KDV
+
+        boolean totalToplam2 = (tutar > 1000);
+        kdvOran = 0.08; // %8 KDV
+
+        String str = totalToplam1 ? "kdvOran = 0.18" : "kdvOran = 0.08" ;
+        System.out.print(str);
+
+
+
+    }
+}
